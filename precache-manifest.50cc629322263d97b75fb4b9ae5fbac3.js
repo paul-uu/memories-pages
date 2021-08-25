e96b2d41d42c7a8c7b833f30ac9b27061d966dcc
Paul <pwittschen@gmail.com>
2021-08-25 09:24:29 -0400

    shuffled around files for pages deploy

---
+++ precache-manifest.50cc629322263d97b75fb4b9ae5fbac3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "608c25ecb324df7ee761b47929bc05c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "35e0240584108a70a14c",
+    "url": "/static/css/2.cb9cc1a5.chunk.css"
+  },
+  {
+    "revision": "22ae8cccab7a2123312b",
+    "url": "/static/css/main.eadbda1c.chunk.css"
+  },
+  {
+    "revision": "35e0240584108a70a14c",
+    "url": "/static/js/2.d6ad5b3e.chunk.js"
+  },
+  {
+    "revision": "5b1c5224559ed5f87ebbaa9023037f2f",
+    "url": "/static/js/2.d6ad5b3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22ae8cccab7a2123312b",
+    "url": "/static/js/main.b34f4271.chunk.js"
+  },
+  {
+    "revision": "cb3ccd3eb684c83ffcee",
+    "url": "/static/js/runtime-main.823c8cbe.js"
+  }
+]);
